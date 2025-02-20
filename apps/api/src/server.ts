3caa2d23bd51015408340cb95ecbad9dba4b5115
karinity0 <karimkhaledelmawe@gmail.com>
2025-02-21 01:51:04 +0200

    chore: update Vercel configuration for monorepo build and logger import

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { json, urlencoded } from "body-parser";
-import logger from "@repo/logger";
+import logger from "../../../packages/logger/dist";
 
 const app = express();
 

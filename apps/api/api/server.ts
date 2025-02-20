366964ad37586fbf641d6df93a15aca9106e2a0c
karinity0 <karimkhaledelmawe@gmail.com>
2025-02-21 01:34:44 +0200

    fix: update logger import to use workspace package alias

--- apps/api/api/server.ts
+++ apps/api/api/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { json, urlencoded } from "body-parser";
-import logger from "../../../packages/logger/dist/index.js";
+import logger from "@repo/logger";
 
 const app = express();
 

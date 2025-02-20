8ece852fb51b02e90000d7645a0945fa898fe7aa
karinity0 <karimkhaledelmawe@gmail.com>
2025-02-20 20:31:31 +0200

    fix: update logger import path in API server

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { json, urlencoded } from "body-parser";
-import { log } from "@repo/logger";
+import { log } from "../../../packages/logger";
 
 const app = express();
 

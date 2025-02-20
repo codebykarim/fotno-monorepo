57b5db4c1597e84e56083f55fbcc43745c65ee8b
karinity0 <karimkhaledelmawe@gmail.com>
2025-02-20 21:49:30 +0200

    chore: update Vercel configuration for Express server deployment

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { json, urlencoded } from "body-parser";
-import { log } from "@repo/logger";
 
 const app = express();
 
@@ -13,7 +12,6 @@ app.use(json());
 const port = process.env.PORT || 5001;
 
 app.get("/", (req: Request, res: Response) => {
-  log("Hello World");
   res.json({
     message: "Hello World",
   });

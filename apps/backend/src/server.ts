e80f05065c8e7139604d102999f5f4981d89db98
karinity0 <karimkhaledelmawe@gmail.com>
2025-02-21 02:07:25 +0200

    chore: restructure backend project with TypeScript and Vercel configuration

---
+++ apps/backend/src/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { json, urlencoded } from "body-parser";
+
+const app = express();
+
+app.use(cors());
+
+app.use(urlencoded({ extended: true }));
+app.use(json());
+
+const port = process.env.PORT || 5001;
+
+app.get("/status", (req: Request, res: Response) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+});

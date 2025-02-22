f7894d2a4d7423fc12a74545394d007bd9a91188
karinity0 <karimkhaledelmawe@gmail.com>
2025-02-23 01:42:16 +0200

    fix: update package name and reset password page type definition

--- apps/web-auth/app/reset-password/page.tsx
+++ apps/web-auth/app/reset-password/page.tsx
@@ -7,7 +7,7 @@ import { redirect } from "next/navigation";
 export default async function ResetPasswordPage({
   searchParams,
 }: {
-  searchParams: { token: string; error: string };
+  searchParams: { token?: string };
 }) {
   const token = searchParams.token;
 
